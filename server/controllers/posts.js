6297abb3c15a374d9dc42852ab3c215b2458988e
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-03 21:06:11 -0400

    Add get and post methods for post messages in the backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,28 @@
-export const getPosts = (req, res) => {
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
     res.send('This Works!');
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new postMessage(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
