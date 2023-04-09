1cb4cea7f338f9520ae359be62488bea120622b3
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-08 23:49:26 -0400

    Add link to home from navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';    // currently causing app to not render because link cannot be outside router
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 
 import useStyles from './styles';
 import header from '../../images/header.png';
@@ -7,12 +8,25 @@ import header from '../../images/header.png';
 const Navbar = () => {
     const classes = useStyles();
 
+    const user = null;  // mock user
+
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography className={classes.heading} variant="h2" align="center">Check Out My Pet!</Typography>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Check Out My Pet!</Typography>
                 <img className={classes.image} src={header} alt="header" height="60" />
             </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageURL}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                )}
+            </Toolbar>
         </AppBar>
     )
 }
