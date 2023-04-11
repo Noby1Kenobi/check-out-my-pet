3192d90dde837d51fcd8a7d6ce3faac5ef3a41e7
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-11 12:01:30 -0400

    Add google login auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 
 import posts from './posts';    // the post function we are importing here is called reducer in the posts.js file
+import auth from './auth';
 
-export default combineReducers({ posts, });
+export default combineReducers({ posts, auth, });
