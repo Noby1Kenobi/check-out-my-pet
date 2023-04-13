dc5b2bc922f92d8a9c8f05af85197aebdd86eac8
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-13 14:18:20 -0400

    Refactor like button to it's own component

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
@@ -12,6 +13,26 @@ import useStyles from './styles';
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    console.log(user);
+    // Like button component
+    const Likes = () => {
+        if (post.likes.length > 0) {
+            // other users liked the post
+            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id)) // check to make sure googleId exists
+                ? (
+                    // user liked the post
+                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
+                ) : (
+                    // user didn't like the post
+                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                );
+        }
+
+        // no one liked the post
+        return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    };
     
     return (
         <Card className={classes.card}>
@@ -33,9 +54,8 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
-                    <ThumbUpAltIcon fontSize="small" />
-                    &nbsp;Like {post.likeCount}
+                <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
+                    <Likes />
                 </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
