2ee588ee8a6893870d5c4810ab3567cb0ed2067d
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-14 06:25:28 -0400

    Add comment post action to backend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -82,7 +82,9 @@ export const likePost = (id) => async (dispatch) => {
 
 export const commentPost = (value, id) => async (dispatch) => {
     try {
-        await api.comment(value, id);
+        const { data } = await api.comment(value, id);
+
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
