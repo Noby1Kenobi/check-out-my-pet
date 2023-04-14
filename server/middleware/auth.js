3629d6652a5834a4c30174541ab1cdb0e7323ffd
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-14 04:06:23 -0400

    Remove ?. when accessing id from decodedtoken

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,12 +11,12 @@ const auth = async (req, res, next) => {
             decodedData = jwt.verify(token, 'test');
 
             // pass user id to next middleware thru the req (specifically lcrud ops in post.js)
-            req.userId = decodedData?.id;
+            req.userId = decodedData.id;    // can no longer use ?. but don't know why
         } else {
             // google token
             decodedData = jwt.decode(token);
 
-            req.userId = decodedData?.sub;
+            req.userId = decodedData.sub;   // can no longer use ?. but don't know why
         }
 
         next();
