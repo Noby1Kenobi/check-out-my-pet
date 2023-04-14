f8ca1bbb94cdae5657deb3ed1c0944106d4d977d
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-13 20:38:56 -0400

    Change state and all cases that use to treat state as only being the array of post objects

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,18 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, START_LOADING, END_LOADING } from '../constants/actionTypes';
 
-// state = { posts: [post objects], currentPage: page number, numberOfPages: number of total pages }
+// state = {
+//     posts: [post objects],
+//     currentPage: number of current page,
+//     numberOfPages: number of total pages,
+//     isLoading: boolean of is the webpage loading content
+// }
 
-const reducer = (state = [], action) => {
+const reducer = (state = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
+        case START_LOADING:
+            return { ...state, isLoading: true };
+        case END_LOADING:
+            return { ...state, isLoading: false };
         case FETCH_ALL:
             return {
                 ... state,
@@ -14,12 +23,12 @@ const reducer = (state = [], action) => {
         case FETCH_BY_SEARCH:
             return { ...state, posts: action.payload, };
         case CREATE:
-            return [...state, action.payload];
+            return { ...state, posts: [...state.posts, action.payload] };
         case UPDATE:
         case LIKE:
-            return state.map((post) => post._id === action.payload._id ? action.payload : post);
+            return { ...state, posts: state.posts.map((post) => post._id === action.payload._id ? action.payload : post) };
         case DELETE:
-            return state.filter((post) => post._id !== action.payload);
+            return { ...state, posts: state.posts.filter((post) => post._id !== action.payload) };
         default:
             return state;
     }
