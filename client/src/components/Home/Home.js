6132c550385f690c87bbde0dd31fa22e633e09c4
Noby1Kenobi <22358324+Noby1Kenobi@users.noreply.github.com>
2023-04-13 20:09:33 -0400

    Seperate all posts into pages so all of the posts are not rendered all at once

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -26,9 +26,9 @@ const Home = () => {
     const [search, setSearch] = useState('');
     const [tags, setTags] = useState([]);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
+    // useEffect(() => {
+    //     dispatch(getPosts());
+    // }, [currentId, dispatch]);
 
     const handleKeyPress = (e) => {
         if(e.keyCode === 13) {  // keyCode 13 = enter
@@ -64,7 +64,7 @@ const Home = () => {
                         </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                         <Paper elevation={6}>
-                            <Pagination />
+                            <Pagination page={page} />
                         </Paper>
                     </Grid>
                 </Grid>
